6041850e16dfe3b044485bd515eea46b9b26da4a
selma <scelosmano1@etf.unsa.ba>
2021-06-21 16:06:18 +0200

    NaN values handled; graph code added

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 from os.path import isfile, join
 import csv
-import matplotlib
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
 
 
 def normalize_data(metric, value):
@@ -28,12 +29,27 @@ def normalize_data(metric, value):
 def handle_data(data):
     i = 0
     for x in data.to_numpy():
-        print(x)
         data.loc[i, 'dit'] = normalize_data(0, x[0])
         data.loc[i, 'rfc'] = normalize_data(1, x[1])
         data.loc[i, 'lcom'] = normalize_data(2, x[2])
         data.loc[i, 'noch'] = normalize_data(5, x[5])
         i = i + 1
+
+
+def make_graph(data, metric):
+    plt.bar(range(data[metric].size), data[metric].to_numpy(), color='#AED6DC')
+    plt.grid(color='#d9d9d9', linestyle='-', linewidth=1, axis='y', alpha=0.7)
+    plt.xlabel('KLASE', color='#737373')
+    plt.ylabel('VRIJEDNOST METRIKE', color='#737373')
+    plt.gca().spines['right'].set_color('none')
+    plt.gca().spines['left'].set_color('none')
+    plt.gca().spines['top'].set_color('none')
+    plt.gca().spines['bottom'].set_color('none')
+
+    plt.show()
+
+
+float_formatter = "{:.3f}".format
 
 
 def print_hi():
@@ -54,7 +70,21 @@ def print_hi():
             data.drop('class', axis='columns', inplace=True)
             data.drop('type', axis='columns', inplace=True)
             handle_data(data)
-            print(data)
+            data.dropna(1, 'all', inplace=True)  # drop kolone u kojima je svaka vrijednost NaN
+            si = SimpleImputer(strategy='mean')
+            data.loc[:, 'dit'] = si.fit_transform(data.loc[:, 'dit'].values.reshape(-1, 1))
+            data.loc[:, 'rfc'] = si.fit_transform(data.loc[:, 'rfc'].values.reshape(-1, 1))
+            data.loc[:, 'lcom'] = si.fit_transform(data.loc[:, 'lcom'].values.reshape(-1, 1))
+            data.loc[:, 'mhf'] = si.fit_transform(data.loc[:, 'mhf'].values.reshape(-1, 1))
+            data.loc[:, 'ahf'] = si.fit_transform(data.loc[:, 'ahf'].values.reshape(-1, 1))
+            data.loc[:, 'noch'] = si.fit_transform(data.loc[:, 'noch'].values.reshape(-1, 1))
+
+            data = data.mean(0)
+            print(data.to_numpy())
+            with open(join(path, 'dataset.csv'), 'a') as file1:
+                np.set_printoptions(formatter={'float_kind': float_formatter})
+                file1.write(','.join(map(str, data.to_numpy())))
+                file1.write("\n")
             i = i + 1
 
 
